8fe599b6d126494e505a16dc5764c5b647f85154
zerone <bazz0324@naver.com>
2025-11-17 19:24:59 +0900

    chore: API 서버 IP 주소 프록시 경로 최신화

--- next.config.mjs
+++ next.config.mjs
@@ -8,19 +8,19 @@ const nextConfig = {
     return [
       {
         source: '/donations/:path*',
-        destination: 'http://localhost:8080/donations/:path*',
+        destination: 'http://43.200.245.141:8080/donations/:path*',
       },
       {
         source: '/api/admin/v1/:path*',
-        destination: 'http://localhost:8080/api/admin/v1/:path*',
+        destination: 'http://43.200.245.141:8080/api/admin/v1/:path*',
       },
       {
         source: '/api/v1/:path*',
-        destination: 'http://localhost:8080/api/v1/:path*',
+        destination: 'http://43.200.245.141:8080/api/v1/:path*',
       },
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8080/api/:path*',
+        destination: 'http://43.200.245.141:8080/api/:path*',
       },
     ]
   },
